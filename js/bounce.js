5b86a7e516df821d1c3f715867ef0cf2950e7f41
Josh <ellisbro58@hotmail.co.uk>
2022-02-28 12:31:46 +0000

    Update bounce.js
    Randomise start position and font colour

--- js/bounce.js
+++ js/bounce.js
@@ -5,9 +5,14 @@ window.addEventListener("load", function() {
 	let region = {x: window.innerWidth, y: window.innerHeight};
 	
 	var velo = {x: 1, y: 1};
-	var pos = {x: 0, y: 0};
+	var pos = {x: Math.floor(Math.random() * (region.x - size.x)), y: Math.floor(Math.random() * (region.y - size.y))};
 	
 	const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+	
+	function randomiseColour() {
+		logo.style.color = "#" + Math.floor(Math.random()*16777215).toString(16);
+	}
+	randomiseColour();
 	
 	setInterval(function() {
 		size = {x: parseInt(style.width), y: parseInt(style.height)};
@@ -15,6 +20,15 @@ window.addEventListener("load", function() {
 		
 		pos.x = clamp(pos.x + velo.x, 0, region.x - size.x);
 		pos.y = clamp(pos.y + velo.y, 0, region.y - size.y);
+		
+		if (pos.x >= region.x - size.x || pos.x <= 0) {
+			velo.x = -velo.x;
+			randomiseColour();
+		}
+		if (pos.y >= region.y - size.y || pos.y <= 0) {
+			velo.y = -velo.y;
+			randomiseColour();
+		}
 		
 		logo.style.left = pos.x + "px";
 		logo.style.top = pos.y + "px";
