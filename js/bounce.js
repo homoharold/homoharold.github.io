53a471016a336534a30e145e271bc8143cc2d705
Josh <ellisbro58@hotmail.co.uk>
2022-02-28 17:46:01 +0000

    Update bounce.js
    cleaned up + stopped text from going off screen

--- js/bounce.js
+++ js/bounce.js
@@ -1,31 +1,35 @@
 window.addEventListener("load", function() {
 	const logo = document.getElementById("bounce");
-	const style = window.getComputedStyle(logo);
-	let size = {x: parseInt(style.width), y: parseInt(style.height)};
-	let region = {x: window.innerWidth, y: window.innerHeight};
+	const style = window.getComputedStyle(logo); // element.style only works for inline css, not external
+
+	let limit = { // maximum position based on viewport size
+		x: window.innerWidth - parseInt(style.width),
+		y: window.innerHeight - (2 * parseInt(style.height))
+	}; 
+	var velo = {x: 1, y: 1}; // current velocity
+	var pos = {x: Math.floor(Math.random() * limit.x), y: Math.floor(Math.random() * limit.y)}; // randomise position inside of limits
 	
-	var velo = {x: 1, y: 1};
-	var pos = {x: Math.floor(Math.random() * (region.x - size.x)), y: Math.floor(Math.random() * (region.y - size.y))};
-	
-	const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+	const clamp = (num, min, max) => Math.min(Math.max(num, min), max); // clamps a number between min and max
 	
 	function randomiseColour() {
-		logo.style.color = "#" + Math.floor(Math.random()*16777215).toString(16);
+		logo.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16); // set colour to random hex value
 	}
 	randomiseColour();
 	
 	setInterval(function() {
-		size = {x: parseInt(style.width), y: parseInt(style.height)};
-		region = {x: window.innerWidth, y: window.innerHeight};
+		limit = {
+			x: window.innerWidth - parseInt(style.width), 
+			y: window.innerHeight - (2 * parseInt(style.height))
+		};
 		
-		pos.x = clamp(pos.x + velo.x, 0, region.x - size.x);
-		pos.y = clamp(pos.y + velo.y, 0, region.y - size.y);
+		pos.x = clamp(pos.x + velo.x, 0, limit.x);
+		pos.y = clamp(pos.y + velo.y, 0, limit.y);
 		
-		if (pos.x >= region.x - size.x || pos.x <= 0) {
+		if (pos.x >= limit.x || pos.x <= 0) {
 			velo.x = -velo.x;
 			randomiseColour();
 		}
-		if (pos.y >= region.y - size.y || pos.y <= 0) {
+		if (pos.y >= limit.y || pos.y <= 0) {
 			velo.y = -velo.y;
 			randomiseColour();
 		}
